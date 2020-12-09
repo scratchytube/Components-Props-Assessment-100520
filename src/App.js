077bc17776f51f116fc16e07558662d0e0d617ec
Brian Sahota <bpsahota@gmail.com>
2020-12-09 08:19:35 -0500

    half done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 import "./App.css";
 import Header from './Components/Header'
 
+// Didnt do the second deliverable. Rewatching the videos and coding along again.
+// Spent most of last night trying to figure out why I kept getting errors while coding along.
+// I kept writing " import React from 'React' " instead of " import React from 'react' " in my Header.js file
+
 class App extends Component {
   render() {
     return (
       <>
-        <header />
+        <Header />
       </>
     )
   }
